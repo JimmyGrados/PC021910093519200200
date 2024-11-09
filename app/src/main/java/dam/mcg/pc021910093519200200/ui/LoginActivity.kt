feeece9435e6528e235949d342f3c3c83f72ab04
Jimmy <jimmygradossalas112@gmail.com>
2024-11-09 11:41:39 -0500

    Login configuration

---
+++ app/src/main/java/dam/mcg/pc021910093519200200/ui/LoginActivity.kt
@@ -0,0 +1,21 @@
+package dam.mcg.pc021910093519200200.ui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import dam.mcg.pc021910093519200200.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
