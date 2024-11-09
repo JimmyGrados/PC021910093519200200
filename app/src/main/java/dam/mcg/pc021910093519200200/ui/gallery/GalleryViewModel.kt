44cfd76b9e7efcce196d366f5710b9d2dfbba8a4
Jimmy <jimmygradossalas112@gmail.com>
2024-11-09 11:11:38 -0500

    Initial commit

---
+++ app/src/main/java/dam/mcg/pc021910093519200200/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package dam.mcg.pc021910093519200200.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
